2206290a4fe96441f0f848a8a44b0dae5c4d363f
Sebastian Salaberria <s_salaberria@hotmail.com>
2020-10-16 13:01:33 -0300

    Added articles app, added CRUD operations/views on articles

--- newspaper_Project/settings.py
+++ newspaper_Project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Local
     'users',
     'pages',
+    'articles',
 
     # 3rd Party
     'crispy_forms',
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
