5d42cd658bba6bb83620d8bf20800d6536befd61
Sebastian Salaberria <s_salaberria@hotmail.com>
2020-10-16 14:13:17 -0300

    Added authorization to CRUD operations

--- articles/views.py
+++ articles/views.py
@@ -3,31 +3,41 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
+    login_url = 'login'  # override LoginRequiredMixin default parameter
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
+    login_url = 'login'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_create.html"
-    fields = ['title', 'body', 'author', ]
+    fields = ['title', 'body', ]
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
-    fields = ['title', 'body',]
+    fields = ['title', 'body', ]
     template_name = "article_edit.html"
+    login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
+    login_url = 'login'
     success_url = reverse_lazy("article_list")
